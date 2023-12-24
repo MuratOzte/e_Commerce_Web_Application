3f6906ca3569945fa9d3932f755f639d0f604695
= <b.b.albayrak@hotmail.com>
2023-12-24 21:22:01 +0300

    Navbar Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Error, Homepage, Layout, LoginPage } from './pages/index.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Error, Homepage, Layout, LoginPage } from "./pages/index.js";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import Products from './pages/Products.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Products from "./pages/Products.jsx";
+import SignUp from "./components/auth/SignUp.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />,
-        errorElement: <Error />,
-        children: [
-            {
-                index: true,
-                element: <Homepage />,
-            },
-            {
-                path: '/products/:id',
-                element: <Products />,
-            },
-            {
-                // path: 'register',
-                // element: <Register />,
-            },
-            {
-                path: 'login',
-                element: <LoginPage />,
-            },
-        ],
-    },
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "register",
+        element: <SignUp />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
