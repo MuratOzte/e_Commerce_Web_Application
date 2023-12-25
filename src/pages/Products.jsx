65e2d94eb93709dfb98f57f7e41cf9c280062ce1
MuratOzte <mo2553481@gmail.com>
2023-12-25 18:31:50 +0300

    authorization added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,66 +1,72 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Products = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
+    const { id } = useParams();
+    const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+    const token = useSelector((state) => state.login.token);
 
-  const baseUrl = "http://localhost:3000/products/";
+    const baseUrl = 'http://localhost:3000/products/';
 
-  useEffect(() => {
-    const fetchedCategories = async () => {
-      try {
-        const response = await fetch(baseUrl + id);
+    useEffect(() => {
+        const fetchedCategories = async () => {
+            try {
+                const response = await fetch(baseUrl + id, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
 
-        if (!response.ok) {
-          throw new Error("Something Went Wrong !");
-        }
+                if (!response.ok) {
+                    throw new Error('Something Went Wrong !');
+                }
 
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-      }
-    };
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
 
-    fetchedCategories();
-  }, [id, baseUrl]);
+        fetchedCategories();
+    }, []);
 
-  return (
-    <>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+    return (
+        <>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles,
+                        with over 6,000 species, ranging across all continents
+                        except Antarctica
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
 
-      <h1>{id}</h1>
-    </>
-  );
+            <h1>{id}</h1>
+        </>
+    );
 };
 
 export default Products;
--- src/store/loginSlice.js
+++ src/store/loginSlice.js
@@ -4,10 +4,14 @@ const loginSlice = createSlice({
     name: 'login',
     initialState: {
         isLoggedIn: false,
+        token: 0,
     },
     reducers: {
         isLoggedInFunc(state) {
             state.isLoggedIn = localStorage.getItem('token');
+        },
+        setToken(state, action) {
+            state.token = action.payload;
         },
     },
 });
