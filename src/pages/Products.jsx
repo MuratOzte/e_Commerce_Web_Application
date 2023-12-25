c739ec84bdb8b3c66294937c8d57fe8c4a0e24d6
= <b.b.albayrak@hotmail.com>
2023-12-25 18:17:27 +0300

    added product card

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,28 +1,66 @@
-import { useParams } from 'react-router-dom';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 const Products = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  const dispatch = useDispatch();
 
-    const baseUrl = 'http://localhost:3000/products/';
+  const baseUrl = "http://localhost:3000/products/";
 
+  useEffect(() => {
     const fetchedCategories = async () => {
+      try {
         const response = await fetch(baseUrl + id);
 
         if (!response.ok) {
-            throw new Error('Something Went Wrong !');
+          throw new Error("Something Went Wrong !");
         }
 
-        return response.json();
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
     };
 
-    const fetchedData = fetchedCategories();
-    console.log(fetchedData);
+    fetchedCategories();
+  }, [id, baseUrl]);
 
-    return (
-        <>
-            <h1>{id}</h1>
-        </>
-    );
+  return (
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+
+      <h1>{id}</h1>
+    </>
+  );
 };
 
 export default Products;
