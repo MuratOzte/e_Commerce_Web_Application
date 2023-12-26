54a81ae37806063c8fbd13da1b7cd0f12ab372b9
= <b.b.albayrak@hotmail.com>
2023-12-26 12:44:58 +0300

    OrderCreate Func Added

--- src/store/loginSlice.js
+++ src/store/loginSlice.js
@@ -1,27 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const loginSlice = createSlice({
-    name: 'login',
-    initialState: {
-        isLoggedIn: localStorage.getItem('token'),
-        token: 0,
-        isModalOpen: false,
-        isCommentModalOpen: false,
+  name: "login",
+  initialState: {
+    isLoggedIn: localStorage.getItem("token"),
+    token: 0,
+    isModalOpen: false,
+    isCommentModalOpen: false,
+    isOrderExist: false,
+  },
+  reducers: {
+    isLoggedInFunc(state) {
+      state.isLoggedIn = localStorage.getItem("token");
     },
-    reducers: {
-        isLoggedInFunc(state) {
-            state.isLoggedIn = localStorage.getItem('token');
-        },
-        setToken(state, action) {
-            state.token = action.payload;
-        },
-        toggleModalOpen(state, action) {
-            state.isModalOpen = action.payload;
-        },
-        toggleCommentModal(state, action) {
-            state.isCommentModalOpen = action.payload;
-        },
+    setToken(state, action) {
+      state.token = action.payload;
     },
+    toggleModalOpen(state, action) {
+      state.isModalOpen = action.payload;
+    },
+    toggleCommentModal(state, action) {
+      state.isCommentModalOpen = action.payload;
+    },
+    OrderExist(state, action) {
+      state.isOrderExist = true;
+    },
+  },
 });
 
 export default loginSlice;
