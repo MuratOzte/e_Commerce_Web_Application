f1ad97144bd99a163ac7ac94057c833469274030
MuratOzte <mo2553481@gmail.com>
2023-12-27 19:06:31 +0300

    comment like redux added

--- src/components/products/CommentBox.jsx
+++ src/components/products/CommentBox.jsx
@@ -1,53 +1,58 @@
-import { useState } from "react";
-import { Paper, Typography, IconButton, Box } from "@mui/material";
-import Avatar from "@mui/material/Avatar";
-import { FavoriteBorder, Favorite } from "@mui/icons-material";
+import { useState } from 'react';
+import { Paper, Typography, IconButton, Box } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import { FavoriteBorder, Favorite } from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import loginSlice from '../../store/loginSlice';
 
 const CommentBox = (props) => {
-  const [isLiked, setIsLiked] = useState(false);
+    const [isLiked, setIsLiked] = useState(false);
+    const dispatch = useDispatch();
+    const commentId = useSelector((state) => state.login.commentId);
 
-  const favoriteChangeHandler = () => {
-    setIsLiked((prev) => !prev);
-  };
+    const favoriteChangeHandler = () => {
+        dispatch(loginSlice.actions.setCommentId(props.commentId));
+        setIsLiked((prev) => !prev);
+    };
 
-  const baseUrl = "http://localhost:3000/auth/like/";
+    const baseUrl = 'http://localhost:3000/auth/like/';
 
-  return (
-    <Paper
-      elevation={8}
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        pl: 5,
-        pr: 8,
-        py: 1,
-        mt: 5,
-        ml: 1,
-      }}
-    >
-      <Box fullwidth display={"flex"} alignItems={"center"}>
-        <Avatar sx={{ bgcolor: "royalblue", mr: 3 }}>M</Avatar>
-        <Typography>{props.comment}</Typography>
-        {!isLiked && (
-          <IconButton
-            onClick={favoriteChangeHandler}
-            style={{ position: "absolute", right: 30 }}
-          >
-            <FavoriteBorder />
-          </IconButton>
-        )}
-        {isLiked && (
-          <IconButton
-            onClick={favoriteChangeHandler}
-            color="error"
-            style={{ position: "absolute", right: 30 }}
-          >
-            <Favorite />
-          </IconButton>
-        )}
-      </Box>
-    </Paper>
-  );
+    return (
+        <Paper
+            elevation={8}
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                pl: 5,
+                pr: 8,
+                py: 1,
+                mt: 5,
+                ml: 1,
+            }}
+        >
+            <Box fullwidth display={'flex'} alignItems={'center'}>
+                <Avatar sx={{ bgcolor: 'royalblue', mr: 3 }}>M</Avatar>
+                <Typography>{props.comment}</Typography>
+                {!isLiked && (
+                    <IconButton
+                        onClick={favoriteChangeHandler}
+                        style={{ position: 'absolute', right: 30 }}
+                    >
+                        <FavoriteBorder />
+                    </IconButton>
+                )}
+                {isLiked && (
+                    <IconButton
+                        onClick={favoriteChangeHandler}
+                        color="error"
+                        style={{ position: 'absolute', right: 30 }}
+                    >
+                        <Favorite />
+                    </IconButton>
+                )}
+            </Box>
+        </Paper>
+    );
 };
 
 export default CommentBox;
--- src/components/products/ProductsComment.jsx
+++ src/components/products/ProductsComment.jsx
@@ -1,79 +1,144 @@
 //packages
 import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from "@mui/material";
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    TextField,
+    InputAdornment,
+    IconButton,
+} from '@mui/material';
 //hooks
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 
 //functions
-import { useEffect, useState } from "react";
-import loginSlice from "../../store/loginSlice";
-import CommentBox from "./CommentBox";
+import { useEffect, useState } from 'react';
+import loginSlice from '../../store/loginSlice';
+import CommentBox from './CommentBox';
+import { Send } from '@mui/icons-material';
 
 const ProductComments = (props) => {
-  const dispatch = useDispatch();
-  const isCommentModalOpen = useSelector(
-    (state) => state.login.isCommentModalOpen
-  );
-  //modal close function
-  const closeBtnHandler = () => {
-    dispatch(loginSlice.actions.toggleCommentModal(false));
-  };
-  const token = useSelector((state) => state.login.token);
-  const id = useSelector((state) => state.login.productId);
-  const baseUrl = "http://localhost:3000/productComments/";
+    const dispatch = useDispatch();
+    //modal close function
+    const isCommentModalOpen = useSelector(
+        (state) => state.login.isCommentModalOpen
+    );
+    const token = useSelector((state) => state.login.token);
+    const id = useSelector((state) => state.login.productId);
+    const baseUrl = 'http://localhost:3000/productComments/';
 
-  const [comments, setComments] = useState(null);
+    const [comments, setComments] = useState(null);
 
-  const createCommentURL =
-    "http://localhost:3000/createProductComment/:productId";
+    const createCommentURL =
+        'http://localhost:3000/createProductComment/:productId';
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!isCommentModalOpen) {
-        return;
-      }
-      try {
-        const response = await fetch(baseUrl + id, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        if (!response.ok) {
-          throw new Error("Something Went Wrong !");
-        }
-
-        const result = await response.json();
-        setComments(result.comments);
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-      }
+    const closeBtnHandler = () => {
+        dispatch(loginSlice.actions.toggleCommentModal(false));
     };
-    fetchData();
-  }, []);
 
-  console.log(comments);
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!isCommentModalOpen) {
+                return;
+            }
+            try {
+                const response = await fetch(baseUrl + id, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                if (!response.ok) {
+                    throw new Error('Something Went Wrong !');
+                }
 
-  return (
-    <Dialog open={isCommentModalOpen} onClose={closeBtnHandler}>
-      <DialogTitle>Comments</DialogTitle>
-      <DialogContent>
-        {console.log(comments)}
-        {comments &&
-          comments.map((e) => (
-            <CommentBox key={e.comment_id} comment={e.comment_text} />
-          ))}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={closeBtnHandler}>close</Button>
-      </DialogActions>
-    </Dialog>
-  );
+                const result = await response.json();
+
+                setComments(result.comments);
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
+        fetchData();
+    }, []);
+
+    // const sendIconHandler = async() => {
+    //     try{
+    //         const response = await fetch(createCommentURL)
+    //     }
+    // }
+
+    console.log(comments);
+
+    return (
+        <Dialog open={isCommentModalOpen} onClose={closeBtnHandler}>
+            <DialogTitle
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    borderBottomColor: 'black',
+                    borderBottom: '2px solid grey',
+                    mb: 1,
+                }}
+            >
+                Comments
+            </DialogTitle>
+            <DialogContent
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignContent: 'center',
+                }}
+            >
+                {Array.isArray(comments) && comments.length > 0 ? (
+                    comments.map((e) => (
+                        <CommentBox
+                            key={e.comment_id}
+                            commentId={e.comment_id}
+                            comment={e.comment_text}
+                        />
+                    ))
+                ) : (
+                    <DialogContentText
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        No comments available.
+                    </DialogContentText>
+                )}
+            </DialogContent>
+            <DialogContent>
+                <TextField
+                    fullWidth
+                    sx={{
+                        boxShadow: 5,
+                        borderRadius: '8px', // Adjust the radius value according to your design
+                    }}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton
+                                    sx={{
+                                        borderRadius: '0 8px 8px 0', // Adjust the radius value accordingly
+                                        boxShadow: 'none', // Apply the same boxShadow value
+                                    }}
+                                >
+                                    <Send />
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={closeBtnHandler}>close</Button>
+            </DialogActions>
+        </Dialog>
+    );
 };
 
 export default ProductComments;
