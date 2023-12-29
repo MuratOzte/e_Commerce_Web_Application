12460a5087f13ca6a23526f81147c95c818bd518
MuratOzte <mo2553481@gmail.com>
2023-12-29 16:06:55 +0300

    cart updated

--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -1,104 +1,130 @@
 //packages
 import {
-  Button,
-  TextField,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from "@mui/material";
+    Button,
+    TextField,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Typography,
+} from '@mui/material';
 //hooks
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 
 //functions
-import loginSlice from "../../store/loginSlice";
-import { useEffect, useState } from "react";
+import loginSlice from '../../store/loginSlice';
+import { useEffect, useState } from 'react';
 
 const OrdersModal = (props) => {
-  const dispatch = useDispatch();
-  const token = useSelector((state) => state.login.token);
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.login.token);
 
-  //modal close function
-  const closeBtnHandler = () => {
-    dispatch(loginSlice.actions.toggleModalOpen(false));
-  };
-  const isOpenRedux = useSelector((state) => state.login.isModalOpen);
-  const isOrderExist = useSelector((state) => state.login.isOrderExist);
-  const [orderId, setOrderId] = useState(null);
-
-  const baseUrl = "http://localhost:3000/createOrder";
-  const orderDetailsUrl = `http://localhost:3000/orderDetails/${orderId}`;
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!isOpenRedux || isOrderExist) {
-        return;
-      }
-      try {
-        dispatch(loginSlice.actions.OrderExist());
-        const response = await fetch(baseUrl, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          method: "POST",
-        });
-
-        if (!response.ok) {
-          throw new Error("Something Went Wrong !");
-        }
-
-        const fetchedData = await response.json();
-        setOrderId(fetchedData.order.order_id);
-        console.log(fetchedData);
-        dispatch(loginSlice.actions.setOrderIndex(fetchedData.order.order_id));
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-      }
+    //modal close function
+    const closeBtnHandler = () => {
+        dispatch(loginSlice.actions.toggleModalOpen(false));
     };
-    fetchData();
-  }, [baseUrl, token, isOpenRedux]);
+    const isOpenModal = useSelector((state) => state.login.isModalOpen);
+    const isOrderExist = useSelector((state) => state.login.isOrderExist);
+    const [orderId, setOrderId] = useState(null);
+    const [orderArray, setOrderArray] = useState(null);
 
-  const orderDetails = async () => {
-    try {
-      const response = await fetch(orderDetailsUrl, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Something went wrong !");
-      }
+    const baseUrl = 'http://localhost:3000/createOrder';
+    const orderDetailsUrl = `http://localhost:3000/orderDetails/${orderId}`;
 
-      const result = await response.json();
-      console.log(result);
-      return result;
-    } catch (error) {
-      console.error("Error fetching data:", error.message);
-    }
-  };
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!isOpenModal || isOrderExist) {
+                return;
+            }
+            try {
+                dispatch(loginSlice.actions.OrderExist());
+                const response = await fetch(baseUrl, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                    method: 'POST',
+                });
 
-  return (
-    <Dialog open={isOpenRedux} onClose={closeBtnHandler}>
-      <DialogTitle>Sepet</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Üniversite e-postanı veya telefon numaranı kullanarak şifreni
-          yenileyebilirsin
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={orderDetails}>asdasd</Button>
-        <Button
-          onClick={() => {
-            closeBtnHandler();
-          }}
-        >
-          İptal
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+                if (!response.ok) {
+                    throw new Error('Something Went Wrong !');
+                }
+
+                const fetchedData = await response.json();
+                setOrderId(fetchedData.order.order_id);
+                console.log('merhaba');
+                console.log(fetchedData);
+                dispatch(
+                    loginSlice.actions.setOrderIndex(fetchedData.order.order_id)
+                );
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
+        fetchData();
+    }, [baseUrl, token, isOpenModal]);
+
+    const orderDetails = async () => {
+        try {
+            const response = await fetch(orderDetailsUrl, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Something went wrong !');
+            }
+
+            const result = await response.json();
+            setOrderArray(result.orderDetails);
+            console.log(result);
+            return result;
+        } catch (error) {
+            console.error('Error fetching data:', error.message);
+        }
+    };
+
+    useEffect(() => {
+        if (orderId) {
+            orderDetails();
+        }
+    }, [isOpenModal]);
+
+    return (
+        <Dialog open={isOpenModal} onClose={closeBtnHandler}>
+            <DialogTitle>Sepet</DialogTitle>
+            <DialogContent>
+                {!orderArray ? (
+                    <p>Loading...</p>
+                ) : (
+                    orderArray.map((e) => (
+                        <DialogContentText
+                            key={e.id}
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                gap: 5,
+                            }}
+                        >
+                            <Typography>{e.product_name}</Typography>
+                            <Typography>{e.price}</Typography>
+                            <Typography>{e.quantity}</Typography>
+                        </DialogContentText>
+                    ))
+                )}
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={orderDetails}>asdasd</Button>
+                <Button
+                    onClick={() => {
+                        closeBtnHandler();
+                    }}
+                >
+                    İptal
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
 };
 
 export default OrdersModal;
