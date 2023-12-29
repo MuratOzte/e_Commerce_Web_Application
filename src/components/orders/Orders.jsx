9fe02046ac835b6a2a7aa14fb01656ede7e319c0
= <b.b.albayrak@hotmail.com>
2023-12-29 12:35:35 +0300

    OrderDetailsApi added

--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -1,83 +1,104 @@
 //packages
 import {
-    Button,
-    TextField,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-} from '@mui/material';
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 //hooks
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
 //functions
-import loginSlice from '../../store/loginSlice';
-import { useEffect, useState } from 'react';
+import loginSlice from "../../store/loginSlice";
+import { useEffect, useState } from "react";
 
 const OrdersModal = (props) => {
-    const dispatch = useDispatch();
-    const token = useSelector((state) => state.login.token);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.login.token);
 
-    //modal close function
-    const closeBtnHandler = () => {
-        dispatch(loginSlice.actions.toggleModalOpen(false));
+  //modal close function
+  const closeBtnHandler = () => {
+    dispatch(loginSlice.actions.toggleModalOpen(false));
+  };
+  const isOpenRedux = useSelector((state) => state.login.isModalOpen);
+  const isOrderExist = useSelector((state) => state.login.isOrderExist);
+  const [orderId, setOrderId] = useState(null);
+
+  const baseUrl = "http://localhost:3000/createOrder";
+  const orderDetailsUrl = `http://localhost:3000/orderDetails/${orderId}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!isOpenRedux || isOrderExist) {
+        return;
+      }
+      try {
+        dispatch(loginSlice.actions.OrderExist());
+        const response = await fetch(baseUrl, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          method: "POST",
+        });
+
+        if (!response.ok) {
+          throw new Error("Something Went Wrong !");
+        }
+
+        const fetchedData = await response.json();
+        setOrderId(fetchedData.order.order_id);
+        console.log(fetchedData);
+        dispatch(loginSlice.actions.setOrderIndex(fetchedData.order.order_id));
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
     };
-    const isOpenRedux = useSelector((state) => state.login.isModalOpen);
-    const isOrderExist = useSelector((state) => state.login.isOrderExist);
+    fetchData();
+  }, [baseUrl, token, isOpenRedux]);
 
-    const baseUrl = 'http://localhost:3000/createOrder';
+  const orderDetails = async () => {
+    try {
+      const response = await fetch(orderDetailsUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Something went wrong !");
+      }
 
-    useEffect(() => {
-        const fetchData = async () => {
-            if (!isOpenRedux || isOrderExist) {
-                return;
-            }
-            try {
-                dispatch(loginSlice.actions.OrderExist());
-                const response = await fetch(baseUrl, {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                    method: 'POST',
-                });
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
 
-                if (!response.ok) {
-                    throw new Error('Something Went Wrong !');
-                }
-
-                const fetchedData = await response.json();
-                console.log(fetchedData);
-                dispatch(
-                    loginSlice.actions.setOrderIndex(fetchedData.order.order_id)
-                );
-            } catch (error) {
-                console.error('Error fetching data:', error.message);
-            }
-        };
-        fetchData();
-    }, [baseUrl, token, isOpenRedux]);
-
-    return (
-        <Dialog open={isOpenRedux} onClose={closeBtnHandler}>
-            <DialogTitle>Sepet</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
-                    Üniversite e-postanı veya telefon numaranı kullanarak
-                    şifreni yenileyebilirsin
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button
-                    onClick={() => {
-                        closeBtnHandler();
-                    }}
-                >
-                    İptal
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+  return (
+    <Dialog open={isOpenRedux} onClose={closeBtnHandler}>
+      <DialogTitle>Sepet</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Üniversite e-postanı veya telefon numaranı kullanarak şifreni
+          yenileyebilirsin
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={orderDetails}>asdasd</Button>
+        <Button
+          onClick={() => {
+            closeBtnHandler();
+          }}
+        >
+          İptal
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
 export default OrdersModal;
