ab4815d342cafef72ac283ac8d68d39c7983224e
MuratOzte <mo2553481@gmail.com>
2024-01-02 12:53:11 +0300

    order bug fixed

--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -1,182 +1,194 @@
 //packages
 import {
-  Button,
-  TextField,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Typography,
-} from "@mui/material";
+    Button,
+    TextField,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Typography,
+} from '@mui/material';
 //hooks
-import { useDispatch, useSelector } from "react-redux";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { useDispatch, useSelector } from 'react-redux';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 //functions
-import loginSlice from "../../store/loginSlice";
-import { useEffect, useState } from "react";
+import loginSlice from '../../store/loginSlice';
+import { useEffect, useState } from 'react';
 
 const OrdersModal = (props) => {
-  const dispatch = useDispatch();
-  const token = useSelector((state) => state.login.token);
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.login.token);
 
-  //modal close function
-  const closeBtnHandler = () => {
-    dispatch(loginSlice.actions.toggleModalOpen(false));
-  };
-  const isOpenModal = useSelector((state) => state.login.isModalOpen);
-  const isOrderExist = useSelector((state) => state.login.isOrderExist);
-  const productId = useSelector((state) => state.login.productId);
-  const [productDeleted, setProductDeleted] = useState(false);
-  const [orderId, setOrderId] = useState(null);
-  const [orderArray, setOrderArray] = useState(null);
-
-  const baseUrl = "http://localhost:3000/createOrder";
-  const orderDetailsUrl = `http://localhost:3000/orderDetails/${orderId}`;
-  const deleteProductFromOrderUrl = `http://localhost:3000/deleteProduct/${orderId}/${productId}`;
-  const deleteOrderUrl = `http://localhost:3000/deleteOrder/${orderId}`;
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!isOpenModal || isOrderExist) {
-        return;
-      }
-      try {
-        dispatch(loginSlice.actions.OrderExist());
-        const response = await fetch(baseUrl, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          method: "POST",
-        });
-
-        if (!response.ok) {
-          throw new Error("Something Went Wrong !");
-        }
-
-        const fetchedData = await response.json();
-        setOrderId(fetchedData.order.order_id);
-        console.log("merhaba");
-        console.log(fetchedData);
-        dispatch(loginSlice.actions.setOrderIndex(fetchedData.order.order_id));
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-      }
+    //modal close function
+    const closeBtnHandler = () => {
+        dispatch(loginSlice.actions.toggleModalOpen(false));
     };
-    fetchData();
-  }, [baseUrl, token, isOpenModal]);
+    const isOpenModal = useSelector((state) => state.login.isModalOpen);
+    const isOrderExist = useSelector((state) => state.login.isOrderExist);
+    const [productDeleted, setProductDeleted] = useState(false);
+    const [orderId, setOrderId] = useState(null);
+    const [orderArray, setOrderArray] = useState(null);
+    const [productIdState, setproductIdState] = useState(null);
 
-  const orderDetails = async () => {
-    try {
-      const response = await fetch(orderDetailsUrl, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Something went wrong !");
-      }
+    const baseUrl = 'http://localhost:3000/createOrder';
+    const orderDetailsUrl = `http://localhost:3000/orderDetails/${orderId}`;
+    const deleteProductFromOrderUrl = `http://localhost:3000/deleteProduct/${orderId}/`;
+    const deleteOrderUrl = `http://localhost:3000/deleteOrder/${orderId}`;
 
-      const result = await response.json();
-      setOrderArray(result.orderDetails);
-      console.log(result);
-      return result;
-    } catch (error) {
-      console.error("Error fetching data:", error.message);
-    }
-  };
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!isOpenModal || isOrderExist) {
+                return;
+            }
+            try {
+                dispatch(loginSlice.actions.OrderExist());
+                const response = await fetch(baseUrl, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                    method: 'POST',
+                });
 
-  useEffect(() => {
-    if (orderId) {
-      orderDetails();
-    }
-  }, [isOpenModal, productDeleted]);
+                if (!response.ok) {
+                    throw new Error('Something Went Wrong !');
+                }
 
-  const deleteProductFromOrder = async () => {
-    try {
-      const response = await fetch(deleteProductFromOrderUrl, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Something went wrong !");
-      }
-      const result = await response.json();
-      console.log(result);
-      return result;
-    } catch (error) {
-      console.error("Error fetching data:", error.message);
-    }
-  };
+                const fetchedData = await response.json();
+                setOrderId(fetchedData.order.order_id);
+                console.log('merhaba');
+                console.log(fetchedData);
+                dispatch(
+                    loginSlice.actions.setOrderIndex(fetchedData.order.order_id)
+                );
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
+        fetchData();
+    }, [baseUrl, token, isOpenModal]);
 
-  const deleteOrder = async () => {
-    try {
-      const response = await fetch(deleteOrderUrl, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Something went wrong !");
-      }
-      const result = await response.json();
-      console.log(result);
-      return result;
-    } catch (error) {
-      console.error("Error fetching data:", error.message);
-    }
-  };
+    const orderDetails = async () => {
+        try {
+            const response = await fetch(orderDetailsUrl, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Something went wrong !');
+            }
 
-  return (
-    <Dialog open={isOpenModal} onClose={closeBtnHandler}>
-      <DialogTitle>Sepet</DialogTitle>
-      <DialogContent>
-        {!orderArray ? (
-          <p>Loading...</p>
-        ) : (
-          orderArray.map((e) => (
-            <DialogContentText
-              key={e.id}
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                gap: 5,
-              }}
-            >
-              <Typography>{e.product_name}</Typography>
-              <Typography>{e.price}</Typography>
-              <Typography>{e.quantity}</Typography>
-              <Button
-                sx={{ marginBottom: 5 }}
-                onClick={() => {
-                  dispatch(loginSlice.actions.setproductId(e.product_id)),
-                    deleteProductFromOrder().then(orderDetails());
-                  console.log("deneme calisiyor " + productId);
-                }}
-              >
-                <DeleteForeverIcon></DeleteForeverIcon>
-              </Button>
-            </DialogContentText>
-          ))
-        )}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={deleteOrder}>Sepeti Temizle</Button>
-        <Button onClick={orderDetails}>Sepet Detayini Yukle</Button>
-        <Button
-          onClick={() => {
-            closeBtnHandler();
-          }}
-        >
-          İptal
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+            const result = await response.json();
+            setOrderArray(result.orderDetails);
+            console.log(result);
+            return result;
+        } catch (error) {
+            console.error('Error fetching data:', error.message);
+        }
+    };
+
+    useEffect(() => {
+        if (orderId) {
+            orderDetails();
+        }
+    }, [isOpenModal, productDeleted]);
+
+    const deleteProductFromOrder = async (productId) => {
+        try {
+            console.log(productId);
+            const response = await fetch(
+                deleteProductFromOrderUrl + productId,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            if (!response.ok) {
+                throw new Error('Something went wrong !');
+            }
+            const result = await response.json();
+            console.log(result);
+            return result;
+        } catch (error) {
+            console.error('Error fetching data:', error.message);
+        }
+    };
+
+    const deleteOrder = async () => {
+        try {
+            const response = await fetch(deleteOrderUrl, {
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Something went wrong !');
+            }
+            const result = await response.json();
+            console.log(result);
+            return result;
+        } catch (error) {
+            console.error('Error fetching data:', error.message);
+        }
+    };
+
+    return (
+        <Dialog open={isOpenModal} onClose={closeBtnHandler}>
+            <DialogTitle>Sepet</DialogTitle>
+            <DialogContent>
+                {!orderArray ? (
+                    <p>Sepet Boş</p>
+                ) : (
+                    orderArray.map((e) => (
+                        <DialogContentText
+                            key={e.id}
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                gap: 5,
+                            }}
+                        >
+                            <Typography>{e.product_name}</Typography>
+                            <Typography>{e.price}</Typography>
+                            <Typography>{e.quantity}</Typography>
+                            <Button
+                                sx={{ marginBottom: 5 }}
+                                onClick={() => {
+                                    deleteProductFromOrder(e.product_id).then(
+                                        setTimeout(() => {
+                                            orderDetails();
+                                        }, 20)
+                                    );
+
+                                    console.log(
+                                        'deneme calisiyor ' + e.product_id
+                                    );
+                                }}
+                            >
+                                <DeleteForeverIcon></DeleteForeverIcon>
+                            </Button>
+                        </DialogContentText>
+                    ))
+                )}
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={deleteOrder}>Sepeti Temizle</Button>
+                <Button onClick={orderDetails}>Sepet Detayini Yukle</Button>
+                <Button
+                    onClick={() => {
+                        closeBtnHandler();
+                    }}
+                >
+                    İptal
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
 };
 
 export default OrdersModal;
